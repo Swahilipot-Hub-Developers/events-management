d7ff15c50153a662ceb2795fa3dbb8a59f0bb778
Jilo007 <146190425+Jilo007@users.noreply.github.com>
2023-11-07 19:06:31 +0000

    adding files

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css'
+import '@/styles/scss/theme.scss'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container mt-5">
+        <h2 className="text-center text-primary">Events Management</h2>
+      </div>
+    </>
+  )
+}
