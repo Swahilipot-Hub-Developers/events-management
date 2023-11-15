f83c5b8524f156cfd8d0191883660de597f04028
jrmugweru <krissfordmwangi@gmail.com>
2023-11-15 17:49:05 +0300

    events -management updated footer and navbar

--- frontend/src/layout/layout.js
+++ frontend/src/layout/layout.js
@@ -1,11 +1,16 @@
-import React from "react";
-import NavBar from "./Navbar";
-import Footer from "./Footer";
+import NavBar from './Navbar';
+import Footer from './Footer';
+import MetaTags from './Metatags';
 
-const Layout = ({}) => {
-    return(
-        <NavBar/>
-    )
+const Layout = ({ children }) => {
+  return (
+    <>
+      <MetaTags/>
+      <NavBar />
+      <main className='container'>{children}</main>
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
